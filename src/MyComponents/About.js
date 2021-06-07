e1648735039f7e336daa00055066c99df0a56145
Raj Kamal Shakya <rajkamalshakya2000@gmail.com>
2021-06-07 08:52:12 +0530

    Deploy my TODOs

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an About component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ut delectus, 
+                sed culpa repellendus quidem, 
+                error voluptatibus quod consectetur tenetur deleniti ea explicabo veniam. Quis pariatur sint quae atque magni.</p>
+        </div>
+    )
+}
